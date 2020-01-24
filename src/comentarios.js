de17e9ae47116922192e23fa38eee0cea1f9ce5f
Flavio <flaviohugo.14@gmail.com>
2020-01-24 19:22:42 -0300

    update README.md

---
+++ src/comentarios.js
@@ -0,0 +1,3 @@
+// Componente: Função que retorna algum HTML, CSS, JS.
+// Propriedade: Informações que o componente PAI passa para o componente FILHO
+// Estado: Informações mantidas pelo componente (Imutabilidade).
